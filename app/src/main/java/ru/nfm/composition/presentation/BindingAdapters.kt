8107223c02e9207fc56851b319332193052f5bed
NFM <daniel.olovianniy@gmail.com>
2023-08-14 04:33:44 +0300

    finished with databinding

--- app/src/main/java/ru/nfm/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/ru/nfm/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package ru.nfm.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import ru.nfm.composition.R
 import ru.nfm.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -58,3 +66,36 @@ private fun getSmileResId(winner: Boolean): Int {
         R.drawable.ic_sad
     }
 }
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, isGoodState: Boolean): Int {
+    val colorResId = if (isGoodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun onOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
+
