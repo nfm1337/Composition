d18636d82eb47602dd37dcc1bb18f2bcba492817
NFM <daniel.olovianniy@gmail.com>
2023-08-14 04:08:00 +0300

    added databinding for GameFinishedFragment

---
+++ app/src/main/java/ru/nfm/composition/presentation/BindingAdapters.kt
@@ -0,0 +1,60 @@
+package ru.nfm.composition.presentation
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import ru.nfm.composition.R
+import ru.nfm.composition.domain.entity.GameResult
+
+@BindingAdapter("requiredAnswers")
+fun bindRequiredAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_score),
+        count
+    )
+}
+
+@BindingAdapter("score")
+fun bindScoreAnswers(textView: TextView, score: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        score
+    )
+}
+
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercent(textView: TextView, percent: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        percent
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
+    }
+}
